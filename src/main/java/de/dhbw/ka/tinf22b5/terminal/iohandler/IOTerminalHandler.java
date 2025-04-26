867b1cee1b3ee21eceb2003df97b01475ba2268a
js-12345 <schaeferjonathan@gmx.net>
2025-04-26 19:54:11 +0200

    removed unused import statements

--- src/main/java/de/dhbw/ka/tinf22b5/terminal/iohandler/IOTerminalHandler.java
+++ src/main/java/de/dhbw/ka/tinf22b5/terminal/iohandler/IOTerminalHandler.java
@@ -1,9 +1,6 @@
 package de.dhbw.ka.tinf22b5.terminal.iohandler;
 
 import de.dhbw.ka.tinf22b5.terminal.exception.TerminalHandlerException;
-import de.dhbw.ka.tinf22b5.terminal.iohandler.lin.LinuxTerminalHandler;
-import de.dhbw.ka.tinf22b5.terminal.iohandler.win.WindowsTerminalHandler;
-import de.dhbw.ka.tinf22b5.util.OSUtil;
 
 import java.awt.*;
 
