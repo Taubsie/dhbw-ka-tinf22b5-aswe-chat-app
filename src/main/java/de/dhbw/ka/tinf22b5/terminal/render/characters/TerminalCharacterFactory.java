0b6dfbf216530280ec48b4a2419911e2cc858cf6
js-12345 <schaeferjonathan@gmx.net>
2025-04-26 22:40:19 +0200

    removed unused generic

--- src/main/java/de/dhbw/ka/tinf22b5/terminal/render/characters/TerminalCharacterFactory.java
+++ src/main/java/de/dhbw/ka/tinf22b5/terminal/render/characters/TerminalCharacterFactory.java
@@ -2,7 +2,7 @@ package de.dhbw.ka.tinf22b5.terminal.render.characters;
 
 public class TerminalCharacterFactory {
 
-    public static <T extends TerminalCharacter> TerminalCharacter[] createTerminalCharactersFromString(String str) {
+    public static TerminalCharacter[] createTerminalCharactersFromString(String str) {
         if (str == null)
             return new TerminalCharacter[0];
 
