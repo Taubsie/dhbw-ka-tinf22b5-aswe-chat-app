acf163d61a47a7254268ba2397972adb0c14f0bf
js-12345 <schaeferjonathan@gmx.net>
2025-04-29 16:20:13 +0200

    using absolute path in execution scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,15 +43,19 @@ task("createProperties") {
     doLast {
         val projectVersion = project.version.toString()
         val jdkName = Jvm.current().javaHome.name ?: "openjdk-23.0.1"
+        val jarFile = projectDir.resolve("build/libs/aswe-chat-app-$projectVersion-all.jar").absolutePath
 
         val executionFile = projectDir.resolve("execution/execute-shadow-jar-develop.sh")
-        executionFile.writeText("java --enable-native-access=ALL-UNNAMED -jar aswe-chat-app-$projectVersion-all.jar")
+        executionFile.parentFile.mkdirs()
+        executionFile.writeText("java --enable-native-access=ALL-UNNAMED -jar $jarFile")
 
         val developmentFile = projectDir.resolve("execution/execute-shadow-jar-develop.bat")
-        developmentFile.writeText("java --enable-native-access=ALL-UNNAMED -jar build/libs/aswe-chat-app-$projectVersion-all.jar")
+        developmentFile.parentFile.mkdirs()
+        developmentFile.writeText("java --enable-native-access=ALL-UNNAMED -jar $jarFile")
 
         val developmentFixedFile = projectDir.resolve("execution/execute-shadow-jar-develop-fixed.bat")
-        developmentFixedFile.writeText("%userprofile%/.jdks/$jdkName/bin/java --enable-native-access=ALL-UNNAMED -jar build/libs/aswe-chat-app-$projectVersion-all.jar")
+        developmentFixedFile.parentFile.mkdirs()
+        developmentFixedFile.writeText("%userprofile%/.jdks/$jdkName/bin/java --enable-native-access=ALL-UNNAMED -jar $jarFile")
 
         val p = Properties()
         p["version"] = projectVersion
