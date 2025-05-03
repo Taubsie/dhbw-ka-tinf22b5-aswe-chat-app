45712d5b4264d6a94c614a3d8692a9036e2766d6
js-12345 <schaeferjonathan@gmx.net>
2025-05-03 20:41:23 +0200

    adding basic datastructures to abstract ui components

--- src/main/java/de/dhbw/ka/tinf22b5/terminal/render/dialog/ChatDialog.java
+++ src/main/java/de/dhbw/ka/tinf22b5/terminal/render/dialog/ChatDialog.java
@@ -1,5 +1,8 @@
 package de.dhbw.ka.tinf22b5.terminal.render.dialog;
 
+import de.dhbw.ka.tinf22b5.chat.Chat;
+import de.dhbw.ka.tinf22b5.chat.Message;
+import de.dhbw.ka.tinf22b5.chat.User;
 import de.dhbw.ka.tinf22b5.terminal.handler.TerminalHandler;
 import de.dhbw.ka.tinf22b5.terminal.key.TerminalKey;
 import de.dhbw.ka.tinf22b5.terminal.key.TerminalKeyEvent;
@@ -8,12 +11,19 @@ import de.dhbw.ka.tinf22b5.terminal.render.layout.ListLayout;
 import de.dhbw.ka.tinf22b5.terminal.render.layout.VerticalSplitLayout;
 
 import java.awt.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class ChatDialog extends Dialog {
 
     private final ListRenderable<ConstSingleLineStringRenderable> userList;
-    private final ListRenderable<ConstSingleLineStringRenderable> chatList;
+    private final ListRenderable<BorderRenderable> chatList;
     private final TextInputRenderable textInput;
+
+    private final List<Chat> chats;
+    int currentChatId = -1;
 
     private Interactable currentlyFocused;
 
@@ -34,11 +44,59 @@ public class ChatDialog extends Dialog {
                 BorderRenderable.BORDER_TOP | BorderRenderable.BORDER_BOTTOM | BorderRenderable.BORDER_LEFT | BorderRenderable.BORDER_RIGHT));
         this.addComponent(new BorderRenderable(panel, BorderRenderable.BorderStyle.DASHED, 1, BorderRenderable.BORDER_LEFT));
 
-        userList.addItem(new ConstSingleLineStringRenderable("User1"));
-        userList.addItem(new ConstSingleLineStringRenderable("User2"));
+        chats = new ArrayList<>();
+        Chat c1 = new Chat(new User("Test1"));
+        chats.add(c1);
+        c1.getMessages().add(0, new Message("Message1", Calendar.getInstance(), true));
+        c1.getMessages().add(0, new Message("Message2", Calendar.getInstance(), false));
+        c1.getMessages().add(0, new Message("Message3", Calendar.getInstance(), false));
+        c1.getMessages().add(0, new Message("Message4", Calendar.getInstance(), true));
 
-        chatList.addItem(new ConstSingleLineStringRenderable("Message 1"));
-        chatList.addItem(new ConstSingleLineStringRenderable("Message 2"));
+        Chat c2 = new Chat(new User("Test2"));
+        chats.add(c2);
+        c2.getMessages().add(0, new Message("Message1", Calendar.getInstance(), false));
+        c2.getMessages().add(0, new Message("Message2", Calendar.getInstance(), false));
+        c2.getMessages().add(0, new Message("Message3", Calendar.getInstance(), false));
+        c2.getMessages().add(0, new Message("Message4", Calendar.getInstance(), true));
+        updateChatUI();
+    }
+
+    public void updateChatUI() {
+        int userSelectedIdx = userList.getSelectedIdx();
+
+        userList.clearItems();
+        userList.setSelectedIdx(userSelectedIdx);
+
+        for (Chat chat : chats) {
+            userList.addItem(new ConstSingleLineStringRenderable(chat.getRemoteUser().getName()));
+        }
+
+
+        if(userList.getItemCount() <= 0 || userList.getSelectedIdx() < 0 || userList.getSelectedIdx() >= userList.getItemCount())
+            return;
+
+        int chatSelectedIdx = chatList.getSelectedIdx();
+        int chatLength = chatList.getItemCount();
+
+        Chat chat = chats.get(userList.getSelectedIdx());
+        chatList.clearItems();
+        for (int i = 0; i < chat.getMessages().size(); i++) {
+            Message message = chat.getMessages().get(i);
+            int borderModifiers = message.isRemoteMessage() ? BorderRenderable.BORDER_RIGHT : BorderRenderable.BORDER_LEFT;
+            borderModifiers |= BorderRenderable.BORDER_BOTTOM;
+
+            String str = message.isRemoteMessage() ? chat.getRemoteUser().getName() : "Me";
+            str += ": " + message.getMessage();
+
+            chatList.addItem(new BorderRenderable(new ConstSingleLineStringRenderable(str), BorderRenderable.BorderStyle.DASHED, 1, borderModifiers));
+        }
+
+        if(currentChatId == userSelectedIdx)
+            chatList.setSelectedIdx(chatList.getItemCount() - chatLength + chatSelectedIdx);
+        else {
+            currentChatId = chatSelectedIdx;
+            chatList.setSelectedIdx(0);
+        }
     }
 
     @Override
@@ -50,9 +108,36 @@ public class ChatDialog extends Dialog {
             case TerminalKey.TK_TAB:
                 doCyclicFocusSwitch();
                 return true;
+            case TerminalKey.TK_ENTER:
+                if (currentlyFocused == textInput) {
+                    sendMessage(terminal);
+                    return true;
+                } else if (currentlyFocused == chatList) {
+                    // TODO: currently not implemented
+                } else if (currentlyFocused == userList) {
+                    updateChatUI();
+                }
+
         }
 
         return false;
+    }
+
+    private void sendMessage(TerminalHandler handler) {
+
+        if (textInput.getText().isBlank())
+            return;
+
+        // TODO: Networking
+        chats.get(userList.getSelectedIdx()).getMessages().add(0, new Message(textInput.getText(), Calendar.getInstance(), false));
+
+        textInput.clearText();
+        updateChatUI();
+        try {
+            handler.updateTerminal();
+        } catch (IOException e) {
+            // ignored
+        }
     }
 
     private void doCyclicFocusSwitch() {
