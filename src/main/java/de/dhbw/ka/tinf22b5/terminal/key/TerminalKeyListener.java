303423344d6a1b2bcf8a6d9bd5f33c1dfea39a4a
js-12345 <schaeferjonathan@gmx.net>
2025-05-03 15:26:50 +0200

    removing unused class

--- src/main/java/de/dhbw/ka/tinf22b5/terminal/key/TerminalKeyListener.java
+++
@@ -1,9 +0,0 @@
-package de.dhbw.ka.tinf22b5.terminal.key;
-
-import de.dhbw.ka.tinf22b5.terminal.handler.TerminalHandler;
-
-import java.io.IOException;
-
-public interface TerminalKeyListener {
-    void keyPressed(TerminalHandler terminal, TerminalKeyEvent terminalKeyEvent) throws IOException;
-}
--- src/main/java/de/dhbw/ka/tinf22b5/terminal/render/dialog/Dialog.java
+++ src/main/java/de/dhbw/ka/tinf22b5/terminal/render/dialog/Dialog.java
@@ -2,17 +2,11 @@ package de.dhbw.ka.tinf22b5.terminal.render.dialog;
 
 import de.dhbw.ka.tinf22b5.terminal.handler.TerminalHandler;
 import de.dhbw.ka.tinf22b5.terminal.key.TerminalKeyEvent;
-import de.dhbw.ka.tinf22b5.terminal.key.TerminalKeyListener;
 import de.dhbw.ka.tinf22b5.terminal.render.components.ContainerRenderable;
 
 import java.io.IOException;
 
-public abstract class Dialog extends ContainerRenderable implements TerminalKeyListener {
+public abstract class Dialog extends ContainerRenderable {
 
     public abstract boolean handleInput(TerminalHandler terminal, TerminalKeyEvent event) throws IOException;
-
-    @Override
-    public final void keyPressed(TerminalHandler terminal, TerminalKeyEvent terminalKeyEvent) throws IOException {
-        handleInput(terminal, terminalKeyEvent);
-    }
 }
